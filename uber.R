fc86431376a2fef42a79f9e8e61503da283d7805
mleibert <mike.leibert@gmail.com>
2019-03-23 15:41:25 -0400

    1

--- uber.R
+++ uber.R
@@ -1,10 +1,13 @@
 setwd("G:\\math\\640")
 
 uber <- read.csv("uber1.csv")
+longlats <- read.csv("longlat.csv")
+
+tail(uber)
 
 length( unique( uber[,1] ) )
 
-install.packages("rjson")
+#install.packages("rjson")
 
 
 library("rjson")
@@ -23,37 +26,18 @@ uberloc[,2] <- as.character(uberloc[,2])
 colnames(uberloc) <- c("ID","address"  )
 head(uberloc)
 
+head(longlats)
+uberloc <- cbind( uberloc, longlats[,2:3] )
+head(uberloc )
 
-uberloc[1,2]
-locs<-list()
-for( i in 1:nrow(uberloc) ){
-locs[[i]] <- data.frame( 
-strsplit(gsub(", ", ",",   uberloc[ i,2], fixed = T), ",",  )[[1]][1],
-strsplit(gsub(", ", ",",   uberloc[ i,2], fixed = T), ",",  )[[1]][2],
-strsplit(gsub(", ", ",",   uberloc[ i,2], fixed = T), ",",  )[[1]][3])
-colnames(locs[[i]]) <- LETTERS[1:3]
-}
-locs<- do.call("rbind", locs)
-
-locs<- apply( locs , 2, as.character) 
-str(locs)
-
-for( i in 1:nrow(locs) ){
-	if( is.na( locs[i,3] ) == F ) { locs[i,2] <- locs[i,3] } }
-	
-locs <- locs[,-3]
-locs <- as.data.frame(locs, stringsAsFactors = F)
- 
-locs$lon <- locs$lat <- rep(1,nrow(locs) )
- 
-locs <- list()
-for( i in 1:nrow(uberloc)){locs[[i]]<-  as.data.frame(
-	geocode( uberloc[i,2]  ))  }
+  
+#for( i in 1:nrow(uberloc)){locs[[i]]<-  as.data.frame(
+#	geocode( uberloc[i,2]  ))  }
 
 
 locss <- do.call("rbind", locs )
 
-write.csv( locss , "longlat.csv")
+#write.csv( locss , "longlat.csv")
 
-#ggmap::register_google(key = "AIzaSyCf2W9UY3uEOtPhJQei74bpJkbo8SpIPdw")
+#
 
